be4b081810d3fecd94e0c61327838b7eaeb9ccb0
Lawrence Hwang <lawrenceh326@gmail.com>
2023-01-11 15:45:43 -0800

    Clearing ROS from JSON helper
    Might not need ros functionality if class only takes input and exports a JSON file.

--- src/wire_manipulation/vision/src/json_output.py
+++ src/wire_manipulation/vision/src/json_output.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
-import rospy
-import ros_numpy
+# import rospy
+# import ros_numpy
 import numpy as np
 from numpy import linalg as la
 import math, json
 
-from geometry_msgs.msg import PoseArray
-from geometry_msgs.msg import Pose
-from visualization_msgs.msg import Marker
-from visualization_msgs.msg import MarkerArray
+# from geometry_msgs.msg import PoseArray
+# from geometry_msgs.msg import Pose
+# from visualization_msgs.msg import Marker
+# from visualization_msgs.msg import MarkerArray
 
-from sklearn.cluster import KMeans
-from wire_modeling.wire_sim import *
-from wire_modeling.wire_grasp_toolbox import WireGraspToolbox, rotm
-from wire_modeling_msgs.srv import ProcessPointCloud, ProcessPointCloudResponse
+# from sklearn.cluster import KMeans
+# from wire_modeling.wire_sim import *
+# from wire_modeling.wire_grasp_toolbox import WireGraspToolbox, rotm
+# from wire_modeling_msgs.srv import ProcessPointCloud, ProcessPointCloudResponse
 
 class JSONOutput():
     def __init__(self):
@@ -31,5 +31,6 @@ if __name__ == "__main__":
     # rospy.init_node('process_point_cloud_server')
     # marker_ = rospy.Publisher('/marker_array', MarkerArray, queue_size=1) # define a publisher marker_array
     # s = rospy.Service('process_point_cloud', ProcessPointCloud, process_point_cloud)
-    print("Exporting JSON of planned trajectory")
-    rospy.spin()
+    # print("Exporting JSON of planned trajectory")
+    # rospy.spin()
+    pass
