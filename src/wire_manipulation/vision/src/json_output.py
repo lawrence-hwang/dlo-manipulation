c10ed801fdd23bcef607929f122b565b78503aca
Lawrence Hwang <lawrenceh326@gmail.com>
2023-01-11 16:08:48 -0800

    Datetime functionality for export + remove ROS

--- src/wire_manipulation/vision/src/json_output.py
+++ src/wire_manipulation/vision/src/json_output.py
@@ -1,21 +1,7 @@
 #!/usr/bin/env python3
-# import rospy
-# import ros_numpy
-# import numpy as np
-# from numpy import linalg as la
-# import math
+
 import json
 from datetime import datetime
-
-# from geometry_msgs.msg import PoseArray
-# from geometry_msgs.msg import Pose
-# from visualization_msgs.msg import Marker
-# from visualization_msgs.msg import MarkerArray
-
-# from sklearn.cluster import KMeans
-# from wire_modeling.wire_sim import *
-# from wire_modeling.wire_grasp_toolbox import WireGraspToolbox, rotm
-# from wire_modeling_msgs.srv import ProcessPointCloud, ProcessPointCloudResponse
 
 class JSONOutput():
     def __init__(self):
@@ -27,13 +13,15 @@ class JSONOutput():
         self.json_result = obj
 
     def export_json(self):
-        with open("sample.json", "w") as outfile:
+        datetime_str = (datetime.now()).strftime("%d-%m-%Y_%H-%M-%S")
+        formatted_filename = "arm-trajectories_" + datetime_str + ".json"
+        with open(formatted_filename, "w") as outfile:
             json.dump(self.json_result, outfile)
 
-if __name__ == "__main__":
+# if __name__ == "__main__":
     # rospy.init_node('process_point_cloud_server')
     # marker_ = rospy.Publisher('/marker_array', MarkerArray, queue_size=1) # define a publisher marker_array
     # s = rospy.Service('process_point_cloud', ProcessPointCloud, process_point_cloud)
     # print("Exporting JSON of planned trajectory")
     # rospy.spin()
-    pass
+    # pass
